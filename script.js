c0b8b69fa5e6870f6b12a7278db903978b7945ae
rachel <rachel@seedsgives.com>
2023-04-06 23:32:50 -0400

    writing pseudocode to begin adding JS functionality

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ formElement.addEventListener("submit", function(event) {
     const inputElement = document.querySelector("input");
     const task = inputElement.value;
 
+});
+
+//trying to figure out some error handling for form submissions.  What should my other
+//JS file linking to the Contact page be called?  What's best practice?  app.js?
+
     //if(task !== ""){  
     //console.log(task);
     //} else {
-      //console.log("they didn't write anything")
+      //pop a message that says "Please complete the form before submitting"?
     //}
+
+
+//Adding a pop-up modal (on page load, or for images, etc)
+
+
+//Making mobile hamburger menu functional
+//at say 960px screen sizes and smaller, the hamburger menu should appear per media query, and the nav links should disappear and flex-direction: column when you click on the hamburger menu.  What do I need to use JS for to make them more "functional" than they are now, since they currently link to the correct pages?  Is it not possible to do that within a hamburger menu using HTML?
